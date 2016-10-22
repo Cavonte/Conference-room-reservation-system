f3f3a9cfcc788b8d453fcd6a8d0c32faa9a05f04
Bruce <edouard.12@live.ca>
2016-10-22 14:19:18 -0400

    Modification to the first iteration of the  ux

--- MobileApplication/SkynetProjectApp/app/src/main/java/com/skynetprojectapp/android/skynetprojectapp/mainActivity.java
+++ MobileApplication/SkynetProjectApp/app/src/main/java/com/skynetprojectapp/android/skynetprojectapp/mainActivity.java
@@ -1,21 +1,22 @@
 package com.skynetprojectapp.android.skynetprojectapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class mainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Button servlet_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +24,16 @@ public class mainActivity extends AppCompatActivity
         setContentView(R.layout.activity_nav__dawer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Header");
-        toolbar.setBackgroundColor(840000);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -43,6 +43,19 @@ public class mainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        servlet_Button = (Button) findViewById(R.id.prototype_testing);
+
+        servlet_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               //this is where we define what the method does.
+                //temporary output to visualize
+
+                Toast.makeText(getApplicationContext(),"Hi, this is Example",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
@@ -55,12 +68,12 @@ public class mainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nav__dawer, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.nav__dawer, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
