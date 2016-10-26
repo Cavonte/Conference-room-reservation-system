e50cecb24586e79cc32d7d0a5fe702c028ab0e95
sihx <telescope72@hotmail.com>
2016-10-26 17:33:16 -0400

    3 - Backend Architectural Pattern: Modify code.

--- MobileApplication/SkynetProjectApp/app/src/main/java/TDG/ReservationTDG.java
+++ MobileApplication/SkynetProjectApp/app/src/main/java/TDG/ReservationTDG.java
@@ -14,29 +14,29 @@ import Core.Reservation;
  */
 
 public class ReservationTDG {
-
+    
     private static String databaseUsername = null;
     private static String databasePassword = null;
-
-
+    
+    
     public ReservationTDG(String username, String password){
-
+        
         this.databaseUsername = username;
         this.databasePassword = password;
-
+        
     }
-
-    public static ArrayList<Reservation> findAll() throws SQLException {
-
+    
+    public ArrayList<Reservation> findAll() throws SQLException {
+        
         ArrayList<Reservation> reservationsList = new ArrayList<>(30);
-
+        
         Connection connection = DriverManager.getConnection("jdbc:mysql:db343.sql", databaseUsername, databasePassword);
         Statement statement = connection.createStatement();
-
+        
         ResultSet resultSet = statement.executeQuery("SELECT * " + "FROM reservations");
-
+        
         while(resultSet.next()){
-
+            
             int resId = resultSet.getInt("reservationId");
             int roomId = resultSet.getInt("roomId");
             int studentId = resultSet.getInt("studentId");
@@ -44,25 +44,25 @@ public class ReservationTDG {
             String startTime = resultSet.getString("startTime");
             String endTime = resultSet.getString("endTime");
             int position = resultSet.getInt("position");
-
+            
             reservationsList.add(new Reservation(roomId, studentId, weekDay, startTime, endTime, position));
         }
-
+        
         resultSet.close();
         statement.close();
         connection.close();
-
+        
         return reservationsList;
-
+        
     }
-
+    
     public static Reservation find(int reservationId) throws SQLException{
-
+        
         Connection connection = DriverManager.getConnection("jdbc:mysql:db343.sql", databaseUsername, databasePassword);
         Statement statement = connection.createStatement();
-
+        
         ResultSet resultSet = statement.executeQuery("SELECT * FROM reservations WHERE reservationId = " + reservationId);
-
+        
         if(resultSet.next()){
             int resId = resultSet.getInt("reservationId");
             int roomId = resultSet.getInt("roomId");
@@ -71,52 +71,52 @@ public class ReservationTDG {
             String startTime = resultSet.getString("startTime");
             String endTime = resultSet.getString("endTime");
             int position = resultSet.getInt("position");
-
+            
             resultSet.close();
             connection.close();
-
+            
             Reservation reservation = new Reservation(roomId, studentId, weekDay, startTime, endTime, position);
-
+            
             return reservation;
         }
         else{
             throw new SQLException("Error: empty result");
         }
-
-
-
+        
+        
+        
     }
-
+    
     public static void insert(Reservation reservation) throws SQLException{
-
+        
         Connection connection = DriverManager.getConnection("jdbc:mysql:db343.sql", databaseUsername, databasePassword);
         Statement statement = connection.createStatement();
-
-        statement.executeUpdate("INSERT INTO reservations " + "VALUES ( " + reservation.getResid() + "," + reservation.getRoomid() + ',' + reservation.getStudentid() + ',' + reservation.getDay() + ',' + reservation.getStartTime() + ',' + reservation.getEndTime() + ',' + reservation.getPosition() + ")");
-
+        
+        statement.executeUpdate("INSERT INTO reservations " + "VALUES ( " + reservation.getResid() + "," + reservation.getRoomid() + "," + reservation.getStudentid() + ", '" + reservation.getDay() + "','" + reservation.getStartTime() + "','" + reservation.getEndTime() + "'," + reservation.getPosition() + ")");
+        
         statement.close();
         connection.close();
     }
-
+    
     public static void update(Reservation reservation) throws SQLException{
-
+        
         Connection connection = DriverManager.getConnection("jdbc:mysql:db343.sql", databaseUsername, databasePassword);
         Statement statement = connection.createStatement();
-
-        statement.executeUpdate("UPDATE reservations " + "SET weekDay = " + reservation.getDay() + ", startTime = " + reservation.getStartTime() + ", endTime = " + reservation.getEndTime() + "WHERE reservationId = " + reservation.getResid());
+        
+        statement.executeUpdate("UPDATE reservations " + "SET weekDay = '" + reservation.getDay() + "', startTime = '" + reservation.getStartTime() + "', endTime = '" + reservation.getEndTime() + "' WHERE reservationId = " + reservation.getResid());
         statement.close();
         connection.close();
     }
-
+    
     public static void delete(Reservation reservation) throws SQLException{
-
+        
         Connection connection = DriverManager.getConnection("jdbc:mysql:db343.sql", databaseUsername, databasePassword);
         Statement statement = connection.createStatement();
-
+        
         statement.executeUpdate("DELETE FROM reservations " + "WHERE reservationId = " + reservation.getResid());
         statement.close();
         connection.close();
-
+        
     }
-
+    
 }
--- MobileApplication/SkynetProjectApp/app/src/main/java/TDG/RoomsTDG.java
+++ MobileApplication/SkynetProjectApp/app/src/main/java/TDG/RoomsTDG.java
@@ -14,102 +14,102 @@ import Core.Room;
  */
 
 public class RoomsTDG {
-
+    
     private static String databaseUsername = null;
     private static String databasePassword = null;
-
-
+    
+    
     public RoomsTDG(String username, String password){
-
+        
         this.databaseUsername = username;
         this.databasePassword = password;
-
+        
     }
-
-    public static ArrayList<Room> findAll() throws SQLException {
-
+    
+    public ArrayList<Room> findAll() throws SQLException {
+        
         ArrayList<Room> rooms = new ArrayList<>(30);
-
+        
         Connection connection = DriverManager.getConnection("jdbc:mysql:db343.sql", databaseUsername, databasePassword);
         Statement statement = connection.createStatement();
-
+        
         ResultSet resultSet = statement.executeQuery("SELECT * " + "FROM rooms");
-
+        
         while(resultSet.next()){
-
+            
             String roomNumber = resultSet.getString("roomNumber");
             String description = resultSet.getString("description");
             int roomSize = resultSet.getInt("roomSize");
-
+            
             rooms.add(new Room(roomNumber, description,roomSize));
         }
-
+        
         resultSet.close();
         statement.close();
         connection.close();
-
+        
         return rooms;
-
+        
     }
-
-    public static Room find(String roomNumber) throws SQLException{
-
+    
+    public static Room find(int roomId) throws SQLException{
+        
         Connection connection = DriverManager.getConnection("jdbc:mysql:db343.sql", databaseUsername, databasePassword);
         Statement statement = connection.createStatement();
-
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM rooms WHERE roomNumber = " + roomNumber);
-
+        
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM rooms WHERE roomId = " + roomId);
+        
         if(resultSet.next()){
             String roomNumber1 = resultSet.getString("roomNumber");
             String description = resultSet.getString("description");
             int roomSize = resultSet.getInt("roomSize");
-
+            
             resultSet.close();
             connection.close();
-
+            
             Room room = new Room(roomNumber1, description, roomSize);
-
+            
             return room;
         }
         else{
             throw new SQLException("Error: empty result");
         }
-
-
-
+        
+        
+        
     }
-
+    
     public static void insert(Room room) throws SQLException{
-
+        
         Connection connection = DriverManager.getConnection("jdbc:mysql:db343.sql", databaseUsername, databasePassword);
         Statement statement = connection.createStatement();
-
-        statement.executeUpdate("INSERT INTO rooms " + "VALUES ( " + room.getRid() + "," + room.getRoomNumber() + ',' + room.getDescription() + ',' + room.getRoomSize() + ")");
-
+        
+        statement.executeUpdate("INSERT INTO rooms " + "VALUES ( " + room.getRid() + ", '" + room.getRoomNumber() + "', '" + room.getDescription() + "'," + room.getRoomSize() + ")");
+        
         statement.close();
         connection.close();
     }
-
+    
     public static void update(Room room) throws SQLException{
-
+        
         Connection connection = DriverManager.getConnection("jdbc:mysql:db343.sql", databaseUsername, databasePassword);
         Statement statement = connection.createStatement();
-
-        statement.executeUpdate("UPDATE rooms " + "SET roomNumber = " + room.getRoomNumber() + ", description = " + room.getDescription() + ", roomSize = " + room.getRoomSize());
+        
+        statement.executeUpdate("UPDATE rooms " + "SET roomNumber = '" + room.getRoomNumber() + "', description = '" + room.getDescription() + "', roomSize = " + room.getRoomSize());
         statement.close();
         connection.close();
     }
-
+    
     public static void delete(Room room) throws SQLException{
-
+        
         Connection connection = DriverManager.getConnection("jdbc:mysql:db343.sql", databaseUsername, databasePassword);
         Statement statement = connection.createStatement();
-
+        
         statement.executeUpdate("DELETE FROM rooms " + "WHERE roomNumber = " + room.getRoomNumber());
         statement.close();
         connection.close();
-
+        
     }
-
-
+    
+    
 }
